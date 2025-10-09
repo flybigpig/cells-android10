1b4e0ccf832dcf0ba1bc7f7b63f0b2ef75bfc330
flybigpig <294722929@qq.com>
2025-10-09 22:14:28 +0800

    servicemanager_service_structure

--- frameworks/native/cmds/servicemanager/bctest.c
+++ frameworks/native/cmds/servicemanager/bctest.c
@@ -14,12 +14,12 @@
  * @param target  目标进程的 handle 值，ServiceManager 对应的 handle 值固定为 0
  * @param name  服务的名字
  */
-uint32_t svcmgr_lookup(struct binder_state *bs, uint32_t target, const char *name)
-{
+uint32_t svcmgr_lookup(struct binder_state *bs, uint32_t target, const char *name) {
     uint32_t handle;
-    unsigned iodata[512/4];
+    unsigned iodata[512 / 4];  // 128 * 4 = 512
     struct binder_io msg, reply;
 
+    // 初始化数据 
     bio_init(&msg, iodata, sizeof(iodata), 4);
     bio_put_uint32(&msg, 0);  // strict mode header
     bio_put_string16_x(&msg, SVC_MGR_NAME);
@@ -28,6 +28,7 @@ uint32_t svcmgr_lookup(struct binder_state *bs, uint32_t target, const char *nam
     //发起远程过程调用
     //调用的方法是  SVC_MGR_CHECK_SERVICE
     // client 端开始休眠
+    // bs msg = io 数据
     if (binder_call(bs, &msg, &reply, target, SVC_MGR_CHECK_SERVICE))
         return 0;
 
@@ -41,10 +42,9 @@ uint32_t svcmgr_lookup(struct binder_state *bs, uint32_t target, const char *nam
     return handle;
 }
 
-int svcmgr_publish(struct binder_state *bs, uint32_t target, const char *name, void *ptr)
-{
+int svcmgr_publish(struct binder_state *bs, uint32_t target, const char *name, void *ptr) {
     int status;
-    unsigned iodata[512/4];
+    unsigned iodata[512 / 4];
     struct binder_io msg, reply;
 
     bio_init(&msg, iodata, sizeof(iodata), 4);
@@ -64,18 +64,18 @@ int svcmgr_publish(struct binder_state *bs, uint32_t target, const char *name, v
 }
 
 unsigned token;
+
 /**
  * 测试程序入口
  * @param argc
  * @param argv
  */
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     struct binder_state *bs;
     uint32_t svcmgr = BINDER_SERVICE_MANAGER;
     uint32_t handle;
 
-    bs = binder_open("/dev/binder", 128*1024);
+    bs = binder_open("/dev/binder", 128 * 1024);
     if (!bs) {
         fprintf(stderr, "failed to open binder driver\n");
         return -1;
@@ -84,27 +84,27 @@ int main(int argc, char **argv)
     argc--;
     argv++;
     while (argc > 0) {
-        if (!strcmp(argv[0],"alt")) {
+        if (!strcmp(argv[0], "alt")) {
             handle = svcmgr_lookup(bs, svcmgr, "alt_svc_mgr");
             if (!handle) {
-                fprintf(stderr,"cannot find alt_svc_mgr\n");
+                fprintf(stderr, "cannot find alt_svc_mgr\n");
                 return -1;
             }
             svcmgr = handle;
-            fprintf(stderr,"svcmgr is via %x\n", handle);
-        } else if (!strcmp(argv[0],"lookup")) {  // 查找
+            fprintf(stderr, "svcmgr is via %x\n", handle);
+        } else if (!strcmp(argv[0], "lookup")) {  // 查找
             if (argc < 2) {
-                fprintf(stderr,"argument required\n");
+                fprintf(stderr, "argument required\n");
                 return -1;
             }
             //获取  服务的 handle 值  handle 值是服务在内核中的索引
             handle = svcmgr_lookup(bs, svcmgr, argv[1]);
-            fprintf(stderr,"lookup(%s) = %x\n", argv[1], handle);
+            fprintf(stderr, "lookup(%s) = %x\n", argv[1], handle);
             argc--;
             argv++;
-        } else if (!strcmp(argv[0],"publish")) {  // 发布
+        } else if (!strcmp(argv[0], "publish")) {  // 发布
             if (argc < 2) {
-                fprintf(stderr,"argument required\n");
+                fprintf(stderr, "argument required\n");
                 return -1;
             }
             // 发布服务
@@ -112,7 +112,7 @@ int main(int argc, char **argv)
             argc--;
             argv++;
         } else {
-            fprintf(stderr,"unknown command %s\n", argv[0]);
+            fprintf(stderr, "unknown command %s\n", argv[0]);
             return -1;
         }
         argc--;
